caac425e66d7f5deb7b3d26c684abc18793fd5fd
Tae Young <k55416325@gamil.com>
2022-02-02 23:15:13 +0900

    Link 연결하려다 어떻하지 싶어서 일단 commit

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -2,6 +2,10 @@ import React from 'react';
 import '../css/ResetCSS.css';
 import '../css/Menubar.css';
 import Home from './Home';
+import Check_attend from './Check_attend';
+import Mypage from './Mypage';
+import Study_main from './Study_main';
+import { Link } from 'react-router-dom';
 
 //태영 : Menubar 제작
 const Menubar = () => {
@@ -32,19 +36,25 @@ const Menubar = () => {
         </ul>
         <div class="burger">
           <ul>
-            <li>마이페이지</li>
-            <li>체크</li>
-            <li>스터디</li>
-            <li>코드라이언</li>
+            <li>
+              <Link to="/">마이페이지</Link>
+            </li>
+            <li>
+              <Link to="/check_task">체크</Link>
+            </li>
+            <li>
+              <Link to="/study_main">스터디</Link>
+            </li>
+            <li>
+              <Link to="">코드라이언</Link>
+            </li>
           </ul>
         </div>
         <div id="logout-imgdiv">
           <img alt="로그아웃 아이콘" src="img/logout_button.png" />
         </div>
       </div>
-      <div class="rightContent">
-        <Home />
-      </div>
+      <div class="rightContent">{}</div>
     </div>
   );
 };
