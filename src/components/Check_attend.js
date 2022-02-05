08dc0a196bca531d14b939a69ef17230d6e6170a
eunbean <ds5bean23@gmail.com>
2022-02-06 00:10:53 +0900

    module 적용

--- src/components/Check_attend.js
+++ src/components/Check_attend.js
@@ -2,33 +2,33 @@
 
 import React from 'react';
 import '../css/ResetCSS.css';
-import '../css/Check_attend.css';
+import styles from '../css/Check_attend.module.css';
 import { Link } from 'react-router-dom';
 
 const Check_task = () => {
   return (
-    <div className='chkbox'>
-        <div className="chkheader">
+    <div className={styles.chkbox}>
+        <div className={styles.chkheader}>
         <Link to = '/Check_task' style={{ textDecoration: 'none' }}>
-        <p className ="tasks">과제 체크</p>
+        <p className ={styles.tasks}>과제 체크</p>
         </Link>
         <Link to = '/Check_attend' style={{ textDecoration: 'none' }}>
-        <p className ="attends">출석 체크</p>
+        <p className ={styles.attends}>출석 체크</p>
         </Link> 
         </div>
-        <p className="chktitle"> 이번주 세션 공지 </p>
-            <hr className="divider" />
-            <img className="speaker"
+        <p className={styles.chktitle}> 이번주 세션 공지 </p>
+            <hr className={styles.divider} />
+            <img className={styles.speaker}
                 alt="스피커 아이콘"
                 src="img/speaker.png"
             />
-            <p className ="choolchk">
+            <p className ={styles.choolchk}>
                 {/* 수정필요 */}
                 동덕여대와 함께하는 GIT 세션
             </p>
-        <div className="Zandi">
+        <div className={styles.Zandi}>
         </div>
-        <p className='DR'>DUKSUNG REPOSITORY</p>
+        <p className={styles.DR}>DUKSUNG REPOSITORY</p>
      </div>
 );
 };
