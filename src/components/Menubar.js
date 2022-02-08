29035a4b89bc36f85d1f98440094b7dc71df54e7
Chanran33 <k55416325@gmail.com>
2022-02-08 17:38:34 +0900

    NavLink를 이용해 Route path별 nav바 스타일 바꾸기(이미지ver. , 텍스트ver. 모두 완료)

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -1,47 +1,63 @@
 import React from 'react';
+import activeMypage from '../img/click_mypage_button.png';
+import activeCheck from '../img/click_check_button.png';
+import activeStudy from '../img/click_study_button.png';
 import '../css/ResetCSS.css';
 import styles from '../css/Menubar.module.css';
-import { Link, Outlet, useLocation } from 'react-router-dom';
+import { Link, Outlet, NavLink } from 'react-router-dom';
 
 //태영 : Menubar 제작
 const Menubar = () => {
-  const location = useLocation();
-
-  const navColorChange = (e) => {
-    switch (location.pathname) {
-      case '/study_main':
-        console.log(location.pathname);
-        break;
-      // no default
-    }
+  const Active = ({ isActive }) => {
+    return { color: isActive ? '#f2921d' : '' };
   };
 
   return (
     <div className={styles.barBody}>
       <div className={styles.leftBar}>
         <div id={styles.homeImgdiv}>
-          <img
-            alt="홈 아이콘"
-            src="img/likelion_DS_logo.png"
-            width="60px"
-            height="60px"
-          />
+          <Link to="/">
+            <img
+              alt="홈 아이콘"
+              src="img/likelion_DS_logo.png"
+              width="60px"
+              height="60px"
+            />
+          </Link>
         </div>
         <ul id={styles.menubar}>
           <li>
-            <Link to="/mypage">
-              <img alt="마이페이지" src="img/mypage_button.png" />
-            </Link>
+            <NavLink
+              to="/mypage"
+              className={styles.mypageLi}
+              style={({ isActive }) => ({
+                backgroundImage: isActive ? `url(${activeMypage})` : '',
+              })}
+            >
+              {/* <img alt="마이페이지" src="img/mypage_button.png" /> */}
+            </NavLink>
           </li>
           <li>
-            <Link to="/check_task">
-              <img alt="체크" src="img/check_button.png" />
-            </Link>
+            <NavLink
+              to="/check_task"
+              className={styles.checkLi}
+              style={({ isActive }) => ({
+                backgroundImage: isActive ? `url(${activeCheck})` : '',
+              })}
+            >
+              {/* <img alt="체크" src="img/check_button.png" /> */}
+            </NavLink>
           </li>
           <li>
-            <Link to="/study_main">
-              <img alt="스터디" src="img/study_button.png" />
-            </Link>
+            <NavLink
+              to="/study_main"
+              style={({ isActive }) => ({
+                backgroundImage: isActive ? `url(${activeStudy})` : '',
+              })}
+              className={styles.studyLi}
+            >
+              {/* <img alt="스터디" src="img/study_button.png" /> */}
+            </NavLink>
           </li>
           <li>
             <a
@@ -49,26 +65,35 @@ const Menubar = () => {
               href="https://www.codelion.net/"
               rel="noreferrer"
             >
-              <img alt="코드라이언" src="img/codelion_button.png" />
+              {/* <img alt="코드라이언" src="img/codelion_button.png" /> */}
+              <div className={styles.codelionLi}></div>
             </a>
           </li>
         </ul>
         <div className={styles.burger}>
           <ul>
-            <li onClick={navColorChange}>
-              <Link className={styles.clicked} to="/mypage">
+            <li>
+              <NavLink style={Active} className={styles.clicked} to="/mypage">
                 마이페이지
-              </Link>
+              </NavLink>
             </li>
-            <li id="check_text" onClick={navColorChange}>
-              <Link className={styles.clicked} to="/check_task">
+            <li id="check_text">
+              <NavLink
+                style={Active}
+                className={styles.clicked}
+                to="/check_task"
+              >
                 체크
-              </Link>
+              </NavLink>
             </li>
-            <li id="study_text" onClick={navColorChange}>
-              <Link className={styles.clicked} to="/study_main">
+            <li id="study_text">
+              <NavLink
+                style={Active}
+                className={styles.clicked}
+                to="/study_main"
+              >
                 스터디
-              </Link>
+              </NavLink>
             </li>
             <li>
               <a
@@ -84,7 +109,8 @@ const Menubar = () => {
         </div>
         <div id={styles.logoutImgdiv}>
           <Link to="/login">
-            <img alt="로그아웃 아이콘" src="img/logout_button.png" />
+            {/* <img alt="로그아웃 아이콘" src="img/logout_button.png" /> */}
+            <div className={styles.logoutLi}></div>
           </Link>
         </div>
       </div>
