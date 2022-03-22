ae38f3ed9c4898c692784741576b9742a4ecbd5e
Tae Young <k55416325@gamil.com>
2022-03-23 03:21:05 +0900

    FEAT: 게시판 LIST VIEW 레이아웃, 반응형 추가

--- src/components/StudyItem.js
+++ src/components/StudyItem.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import studystyle from '../css/Study.module.css';
 
-const StudyItem = () => {
+const StudyItem = ({ study }) => {
   return (
     <div id={studystyle.studyItem}>
       <div id={studystyle.studyItemLeft}>
-        <img alt="스터디 사진 예시" src="img/example1.png"></img>
+        <img alt="스터디 사진 예시" src="img/example1.png" width="300px"></img>
       </div>
       <div id={studystyle.studyItemRight}>
-        <h2>우주 최강 프론트팀</h2>
+        <h2>{study.title}</h2>
         <table>
           <tbody class={studystyle.table}>
             <tr>
               <th>멤버</th>
-              <td>권은빈, 강태영, 김수정, 허은</td>
+              <td>{study.username}</td>
             </tr>
             <tr>
               <th>날짜</th>
-              <td>01/13</td>
+              <td>{study.studyDate}</td>
             </tr>
             <tr>
               <th>내용</th>
               <td>
                 <div>
-                  우주 최강 프론트팀은 오늘도 은하 도장깨기를 하러
-                  가는데눈누난난랄라 룰루 렐리루룰루룰 라라랄랄 라랄 랄라 우주
-                  최강 프론트팀은 오늘도 은하 도장깨기를 하러 가는데눈누난난랄라
-                  룰루 렐리루룰루룰 라라랄랄 라랄 랄라
+                  {study.studyText.length < 50
+                    ? study.studyText
+                    : study.studyText.slice(0, 35) + '...'}
                 </div>
               </td>
             </tr>
--- src/components/Study_main.js
+++ src/components/Study_main.js
@@ -1,8 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import StudyItem from './StudyItem';
 import studystyle from '../css/Study.module.css';
 
 const Study_main = () => {
+  const [studys, setStudys] = useState([
+    {
+      id: 1,
+      title: '우주 최강 프론트팀',
+      username: '권은빈, 강태영, 김수정, 허은',
+      studyDate: '01/13',
+      studyText:
+        '우주 최강 프론트팀은 오늘도 은하 도장깨기를 하러 가는데눈누난난랄라 룰루 렐리루룰루룰 라라랄랄 라랄 랄라 우주 최강 프론트팀은 오늘도 은하 도장깨기를 하러 가는데눈누난난랄라 룰루 렐리루룰루룰 라라랄랄 라랄 랄라',
+    },
+    {
+      id: 2,
+      title: '우주 최강 프론트팀',
+      username: '권은빈, 강태영, 김수정, 허은',
+      studyDate: '01/13',
+      studyText:
+        '우주 최강 프론트팀은 오늘도 은하 도장깨기를 하러 가는데눈누난난랄라 룰루 렐리루룰루룰 라라랄랄 라랄 랄라 우주 최강 프론트팀은 오늘도 은하 도장깨기를 하러 가는데눈누난난랄라 룰루 렐리루룰루룰 라라랄랄 라랄 랄라',
+    },
+    {
+      id: 3,
+      title: '우주 최강 프론트팀',
+      username: 'taeyoung',
+      studyDate: '01/13',
+      studyText:
+        '나도 한때는 그이의 손을 잡고 내가 온 세상 주인공이 된 듯 꽃송이의 꽃잎 하나하나까지 모두 날 위해 피어났지',
+    },
+    {
+      id: 4,
+      title: '우주 최강 프론트팀',
+      username: 'taeyoung',
+      studyDate: '01/13',
+      studyText:
+        '나도 한때는 그이의 손을 잡고 내가 온 세상 주인공이 된 듯 꽃송이의 꽃잎 하나하나까지 모두 날 위해 피어났지',
+    },
+    {
+      id: 5,
+      title: '우주 최강 프론트팀',
+      username: 'taeyoung',
+      studyDate: '01/13',
+      studyText:
+        '나도 한때는 그이의 손을 잡고 내가 온 세상 주인공이 된 듯 꽃송이의 꽃잎 하나하나까지 모두 날 위해 피어났지',
+    },
+    {
+      id: 6,
+      title: '우주 최강 프론트팀',
+      username: 'taeyoung',
+      studyDate: '01/13',
+      studyText:
+        '나도 한때는 그이의 손을 잡고 내가 온 세상 주인공이 된 듯 꽃송이의 꽃잎 하나하나까지 모두 날 위해 피어났지',
+    },
+    {
+      id: 7,
+      title: '우주 최강 프론트팀',
+      username: 'taeyoung',
+      studyDate: '01/13',
+      studyText:
+        '나도 한때는 그이의 손을 잡고 내가 온 세상 주인공이 된 듯 꽃송이의 꽃잎 하나하나까지 모두 날 위해 피어났지',
+    },
+    {
+      id: 8,
+      title: '우주 최강 프론트팀',
+      username: 'taeyoung',
+      studyDate: '01/13',
+      studyText:
+        '나도 한때는 그이의 손을 잡고 내가 온 세상 주인공이 된 듯 꽃송이의 꽃잎 하나하나까지 모두 날 위해 피어났지',
+    },
+    {
+      id: 9,
+      title: '우주 최강 프론트팀',
+      username: 'taeyoung',
+      studyDate: '01/13',
+      studyText:
+        '나도 한때는 그이의 손을 잡고 내가 온 세상 주인공이 된 듯 꽃송이의 꽃잎 하나하나까지 모두 날 위해 피어났지',
+    },
+  ]);
   return (
     <div className={studystyle.studybody}>
       <h1>팀별 스터디 진행 모습</h1>
@@ -11,7 +85,11 @@ const Study_main = () => {
         {/* 조건문으로 운영진일 경우 글쓰기 버튼 생성 */}
         <button id={studystyle.studywrite}>글쓰기</button>
       </div>
-      <StudyItem></StudyItem>
+      <div className={studystyle.contentdiv}>
+        {studys.map((study) => (
+          <StudyItem study={study} key={study.id} />
+        ))}
+      </div>
     </div>
   );
 };
