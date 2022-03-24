b92c60ee3617d57401bab199d9cc2c8ba22755cb
eunbean <ds5bean23@gmail.com>
2022-03-24 23:03:03 +0900

    캘린더 연동 구글 연동

---
+++ src/components/Calendarpg.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import googleCalendarPlugin from '@fullcalendar/google-calendar';
+import '../css/Calendar.css';
+
+// https://fullcalendar.io/docs/google-calendar
+export default function Calendarpg() {
+  const apiKey = process.env.REACT_APP_CAL_API_KEY;
+
+  return (
+    <div className="cal-container">
+      <FullCalendar
+        plugins={[dayGridPlugin, googleCalendarPlugin]}
+        initialView="dayGridMonth"
+        googleCalendarApiKey={apiKey}
+        events={{
+          googleCalendarId: 'ds.likelion@gmail.com',
+        }}
+        eventDisplay={'block'}
+        eventTextColor={'#FFF'}
+        eventColor={'#F2921D'}
+        height={'660px'}
+      />
+    </div>
+  );
+}
